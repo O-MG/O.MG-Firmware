5ba07c72eea0228b4e8eabbf052dd24a8b45ea2c
Wasabi Burns <69662763+spiceywasabi@users.noreply.github.com>
2023-10-07 19:27:42 -0700

    80 char update for beta

--- flash.py
+++ flash.py
@@ -81,121 +81,130 @@ MOTD = """\
 def omg_tos():
     message = """
 Agreement
-O.MG Cable, O.MG Adapter, and O.MG Plug are trademarks of Mischief Gadgets, LLC. Mischief 
-Gadgets, LLC requires that all users read and accept the provisions of the Terms of Use 
-Policy and the Privacy Policy prior to granting users any authorization to use pentesting 
-hardware created by Mischief Gadgets, LLC and/or its affiliates. The Terms of Use Policy 
-and the Privacy Policy can be found at https://o.mg.lol, and must be affirmatively 
-consented to by users prior to using any pentesting hardware created by Mischief Gadgets, 
-LLC and/or its affiliates (hereinafter referred to as “O.MG Devices”). Reading and 
-Accepting the Terms of Use and the Privacy Policy are REQUIRED CONSIDERTIONS for Mischief 
-Gadgets, LLC and/or its affiliates granting users the right to use any O.MG Device. 
-All persons are DENIED permission to use any O.MG Device, unless they read and affirmatively
-accept the Terms of Use Policy and the Privacy Policy located at https://o.mg.lol.
+O.MG Cable, O.MG Adapter, and O.MG Plug are trademarks of Mischief Gadgets, 
+LLC. Mischief Gadgets, LLC requires that all users read and accept the 
+provisions  of the Terms of Use Policy and the Privacy Policy prior to 
+granting users any  authorization to use pentesting hardware created by 
+Mischief Gadgets, LLC  and/or its affiliates. The Terms of Use Policy and
+the Privacy Policy can be  found at  https://o.mg.lol, and must be 
+affirmatively consented to by users prior to using any pentesting hardware
+created by Mischief Gadgets, LLC and/or its  affiliates (hereinafter referred
+to as “O.MG Devices”). Reading and  Accepting the Terms of Use and the Privacy
+Policy are REQUIRED CONSIDERTIONS for Mischief Gadgets, LLC and/or its 
+affiliates granting users the right to use any  O.MG Device. All persons are 
+DENIED permission to use any O.MG Device,  unless they read and affirmatively
+accept the Terms of Use Policy and the Privacy Policy located at
+https://o.mg.lol.
 
 Privacy Policy
-All persons under the age of 18 are denied access to the website located at https://o.mg.lol,
-as well as denied authorization to use any O.MG Device. If you are under the age of 18, it 
-is unlawful for you to visit, communicate, or interact with Mischief Gadgets, LLC and/or 
-its affiliates in any manner. Mischief Gadgets, LLC and/or its affiliates specifically 
-denies access to any individual that is covered by the Child Online Privacy Act (COPA) of 1998.
+All persons under the age of 18 are denied access to the website located at 
+https://o.mg.lol, as well as denied authorization to use any O.MG Device. 
+If you are under the age of 18, it is unlawful for you to visit, communicate, 
+or interact with Mischief Gadgets, LLC and/or its affiliates in any manner. 
+Mischief Gadgets, LLC and/or its affiliates specifically denies access to any
+individual that is covered by the Child Online Privacy Act (COPA) of 1998.
 
-Mischief Gadgets, LLC and/or its affiliates reserve the right to deny access to any person
-or viewer for any reason. Under the provisions of this Privacy Policy, Mischief Gadgets, 
-LLC and/or its affiliates are allowed to collect and store data and information for the 
-purpose of exclusion, and for any other uses seen fit.
+Mischief Gadgets, LLC and/or its affiliates reserve the right to deny access 
+to any person or viewer for any reason. Under the provisions of this Privacy 
+Policy, Mischief Gadgets, LLC and/or its affiliates are allowed to collect and
+store data and information for the purpose of exclusion, and for any other 
+uses seen fit.
 
-Mischief Gadgets, LLC and/or its affiliates have established safeguards to help prevent 
-unauthorized access to or misuse of your information but cannot guarantee that your 
-information will never be disclosed in a manner inconsistent with this Privacy Policy 
-(for example, as a result of any unauthorized act by third parties that violate applicable 
-law or our affiliates’ policies). To protect your privacy and security, we may use passwords
-or other technologies to register or authenticate you and enable you to take advantage of 
-our services, and before granting access or making corrections to your information.
+Mischief Gadgets, LLC and/or its affiliates have established safeguards to 
+help prevent unauthorized access to or misuse of your information but cannot
+guarantee that your information will never be disclosed in a manner 
+inconsistent with this Privacy Policy (for example, as a result of any 
+unauthorized act by third parties that violate applicable law or our
+affiliates’ policies). To protect your privacy and security, we may use 
+passwords or other technologies to register or authenticate you and enable
+you to take advantage  of our services, and before granting access or making
+corrections to your  information.
 
-Mischief Gadgets, LLC and/or its affiliates do not rent or sell your personally identifiable
-information (such as name, address, telephone number, and credit card information) to 
-third parties for their marketing purposes.
+Mischief Gadgets, LLC and/or its affiliates do not rent or sell your personally
+identifiable information (such as name, address, telephone number, and credit 
+card information) to third parties for their marketing purposes.
 
-This Privacy Policy may change from time to time. Users have an affirmative duty, as part
-of the consideration for permission to use O.MG Devices, to keep themselves informed of 
-changes to this Privacy Policy. All changes to this Privacy Policy will be posted
-at https://o.mg.lol.
-
+This Privacy Policy may change from time to time. Users have an affirmative 
+duty, as part of the consideration for permission to use O.MG Devices, to keep 
+themselves informed of changes to this Privacy Policy. All changes to this 
+Privacy Policy will be posted at https://o.mg.lol.
 
 Terms of Use
-Pentesting hardware designed by Mischief Gadgets, LLC and/or its affiliates (hereinafter 
-referred to as “O.MG Devices”) are network administration and pentesting tools used for 
-authorized auditing and security analysis purposes only where permitted, subject to local
-and international laws where applicable. Users are solely responsible for compliance with
-all laws of their locality. Mischief Gadgets, LLC and/or its affiliates claim no 
-responsibility for unauthorized or unlawful use.
+Pentesting hardware designed by Mischief Gadgets, LLC and/or its affiliates 
+(hereinafter referred to as “O.MG Devices”) are network administration and 
+pentesting tools used for authorized auditing and security analysis purposes
+only where permitted, subject to local and international laws where applicable.
+Users are solely responsible for compliance with all laws of their locality. 
+Mischief Gadgets, LLC and/or its affiliates claim no responsibility for 
+unauthorized or unlawful use.
 
-O.MG Devices are packaged with a limited warranty, the acceptance of which is a condition 
-of sale. See https://o.mg.lol for additional warranty details and limitations. 
-Availability and performance of certain features, services, and applications are device 
-and network dependent and may not be available in all areas; additional terms, conditions
-and/or charges may apply.
+O.MG Devices are packaged with a limited warranty, the acceptance of which is
+a condition of sale. See https://o.mg.lol for additional warranty details and
+limitations. Availability and performance of certain features, services, and
+applications are device and network dependent and may not be available in all
+areas; additional terms, conditions and/or charges may apply.
 
-You agree not to access or use any O.MG Device or the website located at https://o.mg.lol 
-in any unlawful way or for any unlawful or illegitimate purpose or in any manner that 
-contravenes this Agreement. You shall not use any O.MG Device to post, use, store, or
-transmit any information that is unlawful, libelous, defamatory, obscene, fraudulent, 
-predatory of minors, harassing, threatening or hateful towards any individual, this
-includes any information that infringes or violates any of the intellectual property 
-rights of others or the privacy rights of others. You shall not use any O.MG Device 
-to attempt to disturb the peace by any method, including through use of viruses, 
-Trojan horses, worms, time bombs, denial of service attacks, flooding or spamming. 
-You shall not use any O.MG Device in any manner that could damage, disable or impair 
-Mischief Gadgets, LLC and/or its affiliates, or any third-party. You shall not use any
-O.MG Device to attempt to gain unauthorized access to any user account, computer systems,
-or networks through hacking, password mining or any other means. You shall not use any 
-O.MG Device alongside any robot, data scraper, miner or virtual computer to gain unlawful
-access to protected computer systems.
+You agree not to access or use any O.MG Device or the website located at 
+https://o.mg.lol in any unlawful way or for any unlawful or illegitimate 
+purpose or in any manner that contravenes this Agreement. You shall not 
+use any O.MG 
 
-All features, functionality and other product specifications are subject to change without
-notice or obligation. Mischief Gadgets, LLC and/or its affiliates reserve the right to
-make changes to the product description in this document without notice. Mischief Gadgets,
-LLC and/or its affiliates do not assume any liability that may occur due to the use or 
-application of the product(s) described herein.
+Device to post, use, store, or transmit any information that is unlawful, 
+libelous, defamatory, obscene, fraudulent, predatory of minors, harassing, 
+threatening or hateful towards any individual, this includes any information 
+that infringes or violates any of the intellectual property rights of others
+or the privacy rights of others. You shall not use any O.MG Device to attempt
+to disturb the peace by any method, including through use of viruses, Trojan 
+horses, worms, time bombs, denial of service attacks, flooding or spamming. 
+You shall not use any O.MG Device in any manner that could damage, disable or
+impair Mischief Gadgets, LLC and/or its affiliates, or any third-party. You 
+shall not use any O.MG Device to attempt to gain unauthorized access to any 
+user account, computer systems, or networks through hacking, password mining 
+or any other means. You shall not use any O.MG Device alongside any robot, data
+scraper, miner or virtual computer to gain unlawful access to protected 
+computer systems.
 
-These terms and conditions shall be governed by and construed in accordance with the laws 
-of the state of New York, United States of America, and you agree to submit to the personal
-jurisdiction of the courts of the state of New York. In the event that any portion of these
-terms and conditions is deemed by a court to be invalid, the remaining provisions shall
-remain in full force and effect. You agree that regardless of any statute or law to the
-contrary, any claim or cause of action arising out of or related to this Web site, or the
-use of this Website, must be filed within one year after such claim or cause of action 
-arose and must be filed in a court in New York, New York, U.S.A.
+All features, functionality and other product specifications are subject to 
+change without notice or obligation. Mischief Gadgets, LLC and/or its 
+affiliates reserve the right to make changes to the product description in 
+this document  without notice. Mischief Gadgets, LLC and/or its affiliates 
+do not assume any liability that may occur due to the use or application of 
+the product(s) described herein.
 
-As required by Section 512(c)(2) of Title 17 of the United States Code, if you believe 
-that any material on the website located at https://o.mg.lol infringes your copyright, 
-you must send a notice of claimed infringement to Mischief Gadget, LLC’s General Counsel 
-at the following address:
+These terms and conditions shall be governed by and construed in accordance 
+with the laws of the state of New York, United States of America, and you 
+agree to  submit to the personal jurisdiction of the courts of the state of 
+New York. In the event that any portion of these terms and conditions is deemed
+by a court to be invalid, the remaining provisions shall remain in full force
+and effect.  You agree that regardless of any statute or law to the contrary,
+any claim or cause of action arising out of or related to this Web site, or the
+use of this Website, must be filed within one year after such claim or cause of 
+action arose and must be filed in a court in New York, New York, U.S.A.
 
+As required by Section 512(c)(2) of Title 17 of the United States Code, if you
+believe that any material on the website located at https://o.mg.lol infringes
+your copyright, you must send a notice of claimed infringement to Mischief 
+Gadget, LLC’s General Counsel  at the following address:
     c/o Mischief Gadgets, LLC - General Counsel
-
     Tor Ekeland Law, PLLC
-
     30 Wall St., 8th Floor
-
     New York, NY 10005
-
     [info@torekeland.com]
+If you do not agree to be bound by this Agreement, do not access or use any 
+O.MG Device,  or the website located at https://o.mg.lol. We reserve the right, 
+with or without notice, to make changes to this Agreement at our discretion. 
+Continued use of any O.MG Device or the website located at https://o.mg.lol 
+constitutes your acceptance of these Terms,  as they may appear at the time 
+of your access.
 
-If you do not agree to be bound by this Agreement, do not access or use any O.MG Device, 
-or the website located at https://o.mg.lol. We reserve the right, with or without notice,
-to make changes to this Agreement at our discretion. Continued use of any O.MG Device or
-the website located at https://o.mg.lol constitutes your acceptance of these Terms, 
-as they may appear at the time of your access.
-
-By continuing, by availing yourself of any O.MG Device or the website located at 
-https://o.mg.lol, or by accessing, visiting, browsing, using or attempting to interact
-with or use any O.MG Device or the website located at https://o.mg.lol, you agree that
-you have read, understand, and agree to be bound by this Agreement as well as our 
-Privacy Policy, which is a part of this Agreement and which can be viewed here: 
-https://o.mg.lol.
+By continuing, by availing yourself of any O.MG Device or the website located 
+at  https://o.mg.lol, or by accessing, visiting, browsing, using or attempting
+to interact with or use any O.MG Device or the website located at 
+https://o.mg.lol, you agree that you have read, understand, and agree to be 
+bound by this Agreement as well as our  Privacy Policy, which is a part of this
+Agreement and which can be viewed here: https://o.mg.lol.
 """
+    
     print("\n\nTo use this flashing tool and O.MG Devices you must agree to the following\n\n")
     try:
         from scripts import pager as pager
